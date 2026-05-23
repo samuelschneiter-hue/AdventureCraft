6af4518150f921b6f6c10d6b7be5e2cfc9070f45
samuelschneiter-hue <samuel.schneiter@gmx.ch>
2026-05-24 01:31:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,6 @@
-import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
+import { defineConfig } from 'vite'
+import tsr from '@tanstack/start/vite'
 
-const config = defineConfig({
-  plugins: [
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tailwindcss(),
-    netlify(),
-    tanstackStart(),
-    viteReact(),
-  ],
-  server: {
-    allowedHosts: [
-      "9a1e74ef-3126-4e8b-ba34-0ff08c5580c0-00-171qbwfocg14e.spock.replit.dev",
-    ],
-  },
-});
-
-export default config;
+export default defineConfig({
+  plugins: [tsr()],
+})
